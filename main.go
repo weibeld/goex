4bf6a6a9f115a6530a755e3348b9447041f787ef
Daniel Weibel <danielmweibel@gmail.com>
2020-04-03 17:20:22 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"./mystring"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(mystring.String())
+}
---
+++ mystring.go
@@ -0,0 +1,6 @@
+// Package mystring
+package mystring
+
+func String() string {
+	return "MyString"
+}
